36f7f0296374399dba303a084a3d473e46481c66
Ethan Andersen <AndersenEthanG@gmail.com>
2021-06-15 11:29:10 -0600

    Initialization

---
+++ Authenticator/Authenticator/Controllers/View Controllers/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  Authenticator
+//
+//  Created by Ethan Andersen on 6/15/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+} // End of Class
